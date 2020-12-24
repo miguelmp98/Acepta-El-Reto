27c0641f21bbc6c4c84b1d4c3192d898cd8c3414
Miguel Pardo Martínez <57708365+miguelmp98@users.noreply.github.com>
2020-12-24 20:25:30 +0100

    Reduciendo envases

---
+++ src/AceptaElReto/n532.java
@@ -0,0 +1,16 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+public class n532 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = in.nextInt(), b = in.nextInt();
+            System.out.println(b - a);
+        }
+    }
+}
