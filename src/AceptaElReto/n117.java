2d9afca46ccb7214f8e7ebc482e8e92692ba6e04
Miguel Pardo Martínez <57708365+miguelmp98@users.noreply.github.com>
2020-12-27 20:37:46 +0100

    La fiesta aburrida

---
+++ src/AceptaElReto/n117.java
@@ -0,0 +1,22 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/*
+ * @autor: miguelmp98
+ * 
+ * */
+public class n117 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for (int i = 0; i < num; i++) {
+			String word = sc.next();
+			String nombre = sc.next();
+			System.out.println("Hola, " + nombre + ".");
+
+		}
+	}
+
+}
